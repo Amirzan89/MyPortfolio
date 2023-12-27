df05441e7c4a8d76bda21122d4719c9a1b60f552
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2023-12-27 20:30:50 +0700

    add vue

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+import { createApp } from '../../node_modules/vue'
+const contact = createApp({
+    template:`
+    `,
+    data(){
+        return{
+            error:''
+        }
+    },
+    methods:{
+        inputEmail() {
+        },
+    }
+});
+contact.mount('#contact');
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,15 @@
-import './bootstrap';
-import { createApp } from 'vue';
-const app = createApp({});
-app.mount('#app');
+import './bootstrap'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
+const app = createApp({
+    data(){
+        return{
+            error:'',
+        }
+    },
+    methods:{
+        inputEmail(){
+            console.log('email inputt');
+        }
+    }
+});
+app.mount('#contact');
